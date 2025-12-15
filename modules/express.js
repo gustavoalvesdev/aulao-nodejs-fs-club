e7a5e6dba09e3a370fcec776902a1b24880151e4
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-12-15 00:43:30 -0300

    Requisições com Express

---
+++ modules/express.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/home', (req, res) => {
+    res.contentType('application/html');
+    res.status(200).send('<h1>Hello World</h1>');
+});
+
+app.get('/users', (req, res) => {
+    const users = [
+        {
+            name: 'John Doe',
+            email: 'john@doe.com'
+        },
+        {
+            name: 'Jane Doe',
+            email: 'jane@doe.com'
+        }
+    ];
+    
+    res.status(200).json(users);
+});
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+    // 57:04
+});
